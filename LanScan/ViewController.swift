59c523962de5a940cb6823039602f877f9eef2b7
Aritro Paul <aritro.paul61@gmail.com>
2019-09-21 23:28:08 +0530

    Initial Commit

---
+++ LanScan/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LanScan
+//
+//  Created by Aritro Paul on 21/09/19.
+//  Copyright © 2019 Aritro Paul. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
